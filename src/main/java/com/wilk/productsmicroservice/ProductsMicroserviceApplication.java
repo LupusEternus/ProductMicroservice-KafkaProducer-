7870c15c6bc023850752b35d382600a289557759
LupusEternus <wilk.tomek@gmail.com>
2024-06-11 12:40:56 +0200

    initial commit

---
+++ src/main/java/com/wilk/productsmicroservice/ProductsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wilk.productsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMicroserviceApplication.class, args);
+	}
+
+}
