ba2bfb16fbc11a2bc4ef3eac2c71fb9cdfd55318
LupusEternus <wilk.tomek@gmail.com>
2024-06-12 15:06:25 +0200

    producer config transfer to java code.

--- src/main/java/com/wilk/productsmicroservice/config/KafkaConfig.java
+++ src/main/java/com/wilk/productsmicroservice/config/KafkaConfig.java
@@ -1,15 +1,28 @@
 package com.wilk.productsmicroservice.config;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wilk.productsmicroservice.service.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServer;
 
     @Bean
     public NewTopic createTopic(){
@@ -19,5 +32,31 @@ public class KafkaConfig {
                 .configs(Map.of("min.insync.replicas","2")) //when msg is sent to this topic,at least 2 replicas must acknowledge
                 .build();
     }
+
+    @Bean
+    public Map<String,Object> producerConfigs(){
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerialize.class);
+        config.put(ProducerConfig.ACKS_CONFIG,"ALL");
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG,120000);
+        config.put(ProducerConfig.LINGER_MS_CONFIG,0);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,3000);
+
+        return config;
+    }
+
+    @Bean
+    public ProducerFactory<String, ProductCreatedEvent> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String,ProductCreatedEvent> kafkaTemplate(){
+        return new KafkaTemplate<String,ProductCreatedEvent>(producerFactory());
+    }
+
+
 
 }
